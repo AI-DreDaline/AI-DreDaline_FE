29f4783393b64695274c7d378ef58b781bcd9ccc
chaeheesong <129745341+chaeheesong@users.noreply.github.com>
2025-10-08 03:30:18 +0900

    Feat: MainScreen 페이지완성
    MainScreen 완성 & RecommendRunScreen(추천경로달리기) 사진 넣기

--- src/navigations/MainNavigator.tsx
+++ src/navigations/MainNavigator.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 // 스크린들
-import LoginScreen from './screens/LoginScreen';
+import RecommendRunScreen from '../screens/MainScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function MainNavigator() {
   return (
-    <Stack.Navigator initialRouteName="Login">
-      <Stack.Screen name="Login" component={LoginScreen} />
+    <Stack.Navigator initialRouteName="RR">
+      <Stack.Screen name="RR" component={RecommendRunScreen} options={{ headerShown: false }} />
     </Stack.Navigator>
   );
 }
