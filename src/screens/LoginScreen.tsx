74ec526dc6bc6a25199af3e608da639945924cd4
chaeheesong <129745341+chaeheesong@users.noreply.github.com>
2025-11-06 23:45:19 +0900

    Feat: 로그인 페이지 완성
    - 회원가입/로그인 화면 완성

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -5,21 +5,28 @@ import { LoingStackParamList } from '../navigations/types';
 
 type Props = NativeStackScreenProps<LoingStackParamList, 'Logo'>;
 
-import mainIcon from '../assets/images/main.png';
+import mainIcon from '../assets/images/mainicon.png';
+import phoneicon from '../assets/images/phone.png';
+import appleicon from '../assets/images/apple.png';
+import navericon from '../assets/images/naver.png';
+import googleicon from '../assets/images/google.png';
+
 
 const LoginScreen = ({ navigation }: Props) => {
     return(
         <View style={styles.container}>
             <Image
                 source={mainIcon}
-                style={{width: 290, height: 270, marginTop: 120, marginLeft: 47}}
+                style={{width: 320, height: 270, marginTop: 120, marginLeft: 40}}
             />
             <View style={styles.buttonview}>
                 <TouchableOpacity
                 >
                     <View style={styles.button}>
-                        <View style={{width:110}}>
+                        <View style={{width:110, paddingLeft: 50, paddingTop: 13}}>
                             <Image
+                                source={phoneicon}
+                                style={{width: 18, height: 25}}
                             />
                         </View>
                         <Text style={styles.buttontext}>휴대폰 번호로 계속하기</Text>
@@ -28,8 +35,10 @@ const LoginScreen = ({ navigation }: Props) => {
                 <TouchableOpacity
                 >
                     <View style={styles.button}>
-                        <View style={{width:110}}>
+                        <View style={{width:110, paddingLeft: 50, paddingTop: 13}}>
                             <Image
+                                source={appleicon}
+                                style={{width: 20, height: 23}}
                             />
                         </View>
                         <Text style={styles.buttontext}>애플 아이디로 계속하기</Text>
@@ -38,8 +47,10 @@ const LoginScreen = ({ navigation }: Props) => {
                 <TouchableOpacity
                 >
                     <View style={styles.button}>
-                        <View style={{width:110}}>
+                        <View style={{width:110, paddingLeft: 50, paddingTop: 13}}>
                             <Image
+                                source={navericon}
+                                style={{width: 23, height: 23}}
                             />
                         </View>
                         <Text style={styles.buttontext}>네이버 아이디로 계속하기</Text>
@@ -48,12 +59,26 @@ const LoginScreen = ({ navigation }: Props) => {
                 <TouchableOpacity
                 >
                     <View style={styles.button}>
-                        <View style={{width:110}}>
+                        <View style={{width:110, paddingLeft: 50, paddingTop: 12}}>
                             <Image
+                                source={googleicon}
+                                style={{width: 22, height: 26}}
                             />
                         </View>
                         <Text style={styles.buttontext}>구글 아이디로 계속하기</Text>
                     </View>
+                </TouchableOpacity>
+            </View>
+            <Text style={styles.bottomtext}>By signing up or singning in, you agree to our</Text>
+            <View style={{flexDirection: 'row', paddingLeft: 95, paddingTop: 7}}>
+                <TouchableOpacity
+                >
+                    <Text style={styles.bottomcolortext}>Terms of Use</Text>
+                </TouchableOpacity>
+                <Text style={[styles.bottomtext, {paddingLeft: 3, paddingRight: 3}]}>and</Text>
+                <TouchableOpacity
+                >
+                    <Text style={styles.bottomcolortext}>Privacy Policy</Text>
                 </TouchableOpacity>
             </View>
         </View>
@@ -68,7 +93,7 @@ const styles = StyleSheet.create ({
     },
     buttonview: {
         width: '100%',
-        height: 308,
+        height: 305,
         marginTop: 81,
         alignItems: 'center',
     },
@@ -87,5 +112,16 @@ const styles = StyleSheet.create ({
         fontWeight:'500',
         paddingTop: 14,
         paddingLeft: 7,
+    },
+    bottomtext: {
+        color: '#ffffff',
+        fontSize: 14,
+        fontWeight: '400',
+        textAlign: 'center',
+    },
+    bottomcolortext: {
+        color: '#39FF14',
+        fontSize: 14,
+        fontWeight: '400',
     },
 });
